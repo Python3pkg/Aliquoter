c3a9247e119afd84258117d08b61f3574f0eb874
Python3pkg <raliclo@gmail.com>
2017-05-17 22:25:41 -0700

    Convert to python3

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from aliquoter import aliquot, Quad, Point
 
-print aliquot(
+print(aliquot(
             Quad(
-                nw=Point(lat=4, long=0),
-                sw=Point(lat=0, long=0),
-                ne=Point(lat=4, long=4),
-                se=Point(lat=0, long=4),
+                nw=Point(lat=4, int=0),
+                sw=Point(lat=0, int=0),
+                ne=Point(lat=4, int=4),
+                se=Point(lat=0, int=4),
             ),
             ['SW', 'SW', 'E']
-        )
+        ))
 
